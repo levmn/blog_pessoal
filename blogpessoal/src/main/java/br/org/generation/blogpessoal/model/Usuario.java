dbe6b233c7461fcfe881506d8de8910c5c022caa
levi <nanowebcontato@gmail.com>
2022-02-03 15:46:06 -0300

    blog pessoal atualizado - swagger

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
@@ -1,14 +1,20 @@
 package br.org.generation.blogpessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "usuario")
@@ -31,6 +37,10 @@ public class Usuario {
 	private String senha;
 	
 	private String foto;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
 	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
 		this.id = id;
