aba0b5802de5d230f88404c0f90f86b40cd5dc9d
levi <nanowebcontato@gmail.com>
2022-02-14 14:01:18 -0300

    criação inicial do react do blog pessoal

---
+++ front_end/blog_pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        // fragmento
+        <> 
+            <h1 className="titulo">Home</h1>
+            <img src="https://imgur.com/HHHzWIS.png" alt="Imagem tela inicial." className="img"/>
+        </>
+    );
+}
+
+export default Home;
